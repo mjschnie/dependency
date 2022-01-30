6319dc045382689a7c025b26078df915f2c71a87
Michael J. Schnieders <michael-schnieders@uiowa.edu>
2022-01-30 11:04:01 -0600

    Update groovy-console-4.0.0-ffx

--- org/apache/groovy/groovy-console/update.sh
+++ org/apache/groovy/groovy-console/update.sh
@@ -6,13 +6,13 @@ cd $VER-ffx
 
 jar xvf groovy-console-$VER.jar
 
-cp ../MacOSXMenuBar.groovy groovy/console/ui/view/.
+cp ../MacOSXMenuBar.groovy .
 
-goovyc groovy/console/ui/view/MacOSXMenuBar.groovy
+goovyc MacOSXMenuBar.groovy
 
 jar cvf groovy-console-$VER-ffx.jar META-INF groovy
 
-rm -rf META-INF groovy groovy-console-$VER.jar
+rm -rf META-INF groovy MacOSXMenuBar.groovy groovy-console-$VER.jar
 
 mv groovy-console-$VER.pom groovy-console-$VER-ffx.pom
 
